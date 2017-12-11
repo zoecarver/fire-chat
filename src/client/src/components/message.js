f9d90b521e1bf334946add249e6ba18f59e1abc4
zoe <z.zoelec2@gmail.com>
2017-12-11 07:50:23 -0800

    Fix message sender undifined

--- src/client/src/components/message.js
+++ src/client/src/components/message.js
@@ -5,7 +5,7 @@ export default class Msg extends Component {
   render() {
     return (
       <div class="pt-card pt-elevation-0 pt-interactive">
-        <span className="pt-tag pt-round">{this.props.sender.displayName}</span>
+        <span className="pt-tag pt-round">{this.props.sender ? this.props.sender.displayName : ''}</span>
         <p>{this.props.text}</p>
       </div>
     );
